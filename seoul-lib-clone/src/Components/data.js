a417b0c3336b7a8856da3775fc0a623896f6fd26
ybkim0846 <ububin@gmail.com>
2023-10-15 19:28:19 +0900

    add images

--- seoul-lib-clone/src/Components/data.js
+++ seoul-lib-clone/src/Components/data.js
@@ -1,9 +1,13 @@
 import pic1 from '../img/메인 1.jpg'
-// import pic2 from '../img/GNB바.png'
-// import pic3 from '../img/전사도서관 상세페이지.png'
-// import pic4 from '../img/전자도서관 메인.png'
+import pic2 from '../img/GNB바.jpg'
+import pic3 from '../img/전자도서관 상세페이지 1.jpg'
+import pic4 from '../img/전자도서관 메인 1.jpg'
+import pic5 from '../img/전자도서관 검색결과.jpg'
+import pic6 from '../img/도서 상세페이지.jpg'
+import pic7 from '../img/대출 안내창.jpg'
+import pic8 from '../img/내서재 이동 안내창.jpg'
 
-const data = [ pic1 ];
+const data = [ pic1, pic2, pic3, pic4, pic5, pic6, pic7, pic8 ];
 
 const ImgData = (num) => {
     const cur = num.num;
@@ -11,9 +15,6 @@ const ImgData = (num) => {
     return (
         <div>
             <img src={data[cur]} alt='' width="100%" />
-            {/* { data.map ((pic, index)=> {
-                return <img src={pic} alt='' key={index}/>
-            })} */}
         </div>
     )
 }
