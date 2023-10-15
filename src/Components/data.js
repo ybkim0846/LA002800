dd28b352bd08792eadc92b610bbb703b23478229
ybkim0846 <ububin@gmail.com>
2023-10-16 00:59:01 +0900

    북리뷰 이미지 추가

--- src/Components/data.js
+++ src/Components/data.js
@@ -22,8 +22,20 @@ import pic19 from '../img/Viewer/하단 스크롤을 통해 2페이지로 이동
 import pic20 from '../img/Viewer/왼쪽 상단 엑스표 눌러 종료.jpg'
 
 
+//review 부분
+import pic21 from '../img/review/내서재.jpg'
+import pic22 from '../img/review/반납 안내창.jpg'
+import pic23 from '../img/review/반납 완료 안내창.jpg'
+import pic24 from '../img/review/전자도서관 검색결과.jpg'
+import pic25 from '../img/review/도서 상세페이지.jpg'
+import pic26 from '../img/review/북리뷰 작성.jpg'
+import pic27 from '../img/review/리뷰 등록 안내창.jpg'
+import pic28 from '../img/review/북리뷰 수정 및 삭제 페이지.jpg'
+import pic29 from '../img/review/북리뷰 삭제 1.jpg'
+import pic30 from '../img/review/북리뷰 삭제 완료 안내창 1.jpg'
 
-const data = [ pic1, pic2, pic3, pic4, pic5, pic6, pic7, pic8, pic9, pic10, pic11, pic12, pic13, pic14, pic15, pic16, pic17, pic18, pic19, pic20 ];
+
+const data = [ pic1, pic2, pic3, pic4, pic5, pic6, pic7, pic8, pic9, pic10, pic11, pic12, pic13, pic14, pic15, pic16, pic17, pic18, pic19, pic20, pic21, pic22, pic23, pic24, pic25, pic26, pic27, pic28, pic29, pic30 ];
 
 const ImgData = (num) => {
     const cur = num.num;
